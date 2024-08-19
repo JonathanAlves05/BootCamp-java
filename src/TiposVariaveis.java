eef51c46d34fc96f63afcd9e39316e2f6d5c348e
JonathanAlves05 <jarprodrigues05@gmail.com>
2024-08-19 16:40:18 -0300

    subindo

---
+++ src/TiposVariaveis.java
@@ -0,0 +1,39 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+         
+        // Tipos de Variáveis em Java
+
+// 1. Inteiros
+int idade = 18; // Armazena números inteiros, como 18, -5, 0. 
+
+byte numPequeno = 100; // Armazena números inteiros menores.
+
+short numMedio = 32000; // Armazena números inteiros médios.
+
+long numGrande = 100000L; // Armazena números inteiros grandes. 
+
+// 2. Ponto Flutuante
+float altura = 1.75f; // Armazena números decimais. 
+
+double peso = 70.5; // Armazena números decimais com maior precisão.
+
+// 3. Caracteres
+char inicial = 'J'; // Armazena um único caractere. 
+
+// 4. Booleanos
+boolean aprovado = true; // Armazena valores verdadeiros ou falsos. 
+
+// 5. String
+String nome = "Jonathan"; // Armazena cadeias de caracteres (texto). 
+
+ 
+ System.out.println("Nome: " + nome);
+ System.out.println("Idade: " + idade);
+ System.out.println("Altura: " + altura);
+ System.out.println("Peso: " + peso);
+ System.out.println("Inicial: " + inicial);
+ System.out.println("Aprovado: " + aprovado);
+    }
+   
+    
+}
